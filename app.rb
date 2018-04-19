1058a721ac6009636aa48afa475eb2ba50e7ebb3
Lukas Carling <lukas.carling@itggot.se>
2018-04-19 09:13:46 +0200

    Update

--- app.rb
+++ app.rb
@@ -9,4 +9,31 @@ class App < Sinatra::Base
 		erb(:index)
 	end
 
+	get ('/register') do
+		erb(:register)
+	end
+
+
+
+	post ('/register') do
+
+		username = params[:username]
+		password = params[:password]
+		nickname = params[:nickname]
+
+		if username != nil 
+			if user_compare(username).empty? == true
+				if params[:password]==params[:second_password]
+
+					crypt_pass = BCrypt::Password.create(password)
+					create_user(username, crypt_pass, nickname)
+				end
+			end
+		else
+				error = "Please register correctly!"
+				redirect('/register')
+		end
+		redirect('/register')
+	end
+
 end           
--- module.rb
+++ module.rb
@@ -1,3 +1,30 @@
 module TodoDB
 
+    DB_PATH = './database/database.sqlite'
+
+    def db_connect()
+        db = SQLite3::Database.new(DB_PATH)
+        db.results_as_hash = true
+        return db
+    end
+        
+    def user_compare(username)
+        db = db_connect()
+        result = db.execute("SELECT * FROM users WHERE username = ?",[username])
+        return result
+    end
+
+    def create_user(username, crypt_pass, nickname)
+        if nickname == nil
+            nickname = "This user has not set a nickname yet"
+        end
+
+        db = db_connect()
+        request = db.execute("INSERT INTO users('username', 'password', 'nickname') VALUES(?,?,?)", [username, crypt_pass, nickname])
+        return request
+    end
+
+
+
+
 end
